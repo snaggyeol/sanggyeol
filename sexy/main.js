12fca92c0984a7ae9e02bc0c50343c330b089238
snaggyeol <163866591+snaggyeol@users.noreply.github.com>
2024-03-26 15:20:57 +0900

    빨갱이
    빨간 장애물

--- sexy/main.js
+++ sexy/main.js
@@ -1,8 +1,35 @@
-var canvas = document.getElementById('canvas');
-var ctx = canvas.getContext('2d');
+var canvas=document.getElementById('canvas');
+var ctx=canvas.getContext('2d');
 
-canvas.width = window.innerWidth - 100;
-canvas.height = window.innerHeight - 100;
+canvas.width=window.innerWidth-100;
+canvas.height=window.innerHeight-100;
 
-ctx.fillStyle = 'green';
-ctx.fillRect(800,150,1000,1000);
+var user = {
+    x:10,
+    y:10,
+    width:100,
+    height:100,
+
+    draw(){
+        ctx.fillStyle = 'green';
+        ctx.fillRect(this.x,this.y,this.width,this.height);
+    }
+}
+
+user.draw()
+
+class Bomb{ //장애물
+constructor(){
+    this.x = 300;
+    this.y = 10;
+    this.width = 20;
+    this.height = 20;
+}
+draw(){
+    ctx.fillStyle = 'red';
+    ctx.fillRect( this.x, this.x,this.width,this.height);
+}
+}
+ 
+var bomb = new Bomb();
+bomb.draw() 
